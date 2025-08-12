598a8b359032ec8df0a106b164988f19c38d6024
mikeag96 <miguelalopezg96@gmail.com>
2025-08-12 11:23:22 -0600

    chore: add hexagonal architecture package to evaluate project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import hexagonal from 'eslint-plugin-hexagonal-architecture';
 
 export default tseslint.config(
   {
@@ -11,6 +12,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  // General rules
   {
     languageOptions: {
       globals: {
@@ -23,12 +25,32 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+  // Hexagonal Architecture Rules
+  {
+    files: ['src/**/*.ts'],
+    plugins: {
+      'hexagonal-architecture': hexagonal,
+    },
+    settings: {
+      hexagonal: {
+        rootDir: 'src',
+        layers: ['application', 'domain', 'infrastructure'],
+      },
+    },
+    rules: {
+      'hexagonal-architecture/enforce': 'error',
+    },
+  },
+  {
+    files: ['src/app.module.ts', 'src/main.ts'],
+    rules: {
+      'hexagonal-architecture/enforce': 'off',
     },
   },
 );
